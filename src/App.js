7e71623c80887f2fd2308e58ccc6f97211acbaa1
will henrique <willhc1984@gmail.com>
2023-01-10 15:41:50 -0300

    Busca de dados com useEffect / fetch

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import SearchInput from './Components/SearchInput';
+
+const api = 'https://kitsu.io/api/edge/';
 
 function App() {
+
+  const [text, setText] = useState('');
+  const [info, setInfo] = useState({});
+  
+  useEffect(() => {
+    if(text){
+      fetch(`${api}anime?filter[text]=${text}&page[limit]=10`)
+      .then((response) => response.json())
+      .then((response) => {
+        setInfo(response);
+        console.log(info);
+      });
+    }
+  }, [text]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Animes</h1>
+      <SearchInput value={text} onChange={(search) => setText(search)} />
+      {info.data && (
+        <ul className='animes-list'>
+          {info.data.map((anime) => (
+            <li key={anime.id}>
+              <img src={anime.attributes.posterImage.small} alt={anime.attributes.canonicalTitle} />
+              {anime.attributes.canonicalTitle}
+            </li>
+          ))}
+          
+        </ul>
+      )}
     </div>
   );
 }
---
+++ src/Components/SearchInput.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const SearchInput = ({value, onChange}) => {
+
+    function handleChange(event){
+        onChange(event.target.value);
+    }
+
+    return(
+        <input type="search" value={value} onChange={handleChange} />
+    )
+
+}
+
+export default SearchInput;
